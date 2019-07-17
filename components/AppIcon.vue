b33a61ddc7cbe4be90ae6bad0dfc261650e5b20e
Amir Momenian <nainemom@gmail.com>
2019-07-17 17:22:42 +0430

    create AppIcon component

---
+++ components/AppIcon.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="icon" />
+</template>
+
+<style scoped>
+.icon {
+  display: inline-block;
+  background-image: url('/icon.png');
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  width: 300px;
+  height: 300px;
+}
+</style>
