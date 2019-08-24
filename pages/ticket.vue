bf907434cfcd2e54e940ca5e48ea13d1d3644342
HamidNE <hamidne@mail.ru>
2019-08-24 23:47:59 +0430

    fixed evand script codes

--- pages/ticket.vue
+++ pages/ticket.vue
@@ -2,26 +2,22 @@
   <div class="container py-15">
     <div
       class="evand-widget evand-widget-event-registration"
-      data-event-slug="test-88342248"
-      data-setting
-    ></div>
+      data-event-slug="test-88342248" data-setting></div>
   </div>
 </template>
 
 <script>
 export default {
-  mounted() {
-    if (!document.getElementById("evand-widget-event-registration")) {
-      const t = document.createElement("script");
-      t.type = "text/javascript";
-      t.async = !0;
-      t.src =
-        ("https:" == document.location.protocol ? "https://" : "http://") +
-        "widgets.evand.com/event-registration.js?load=1";
-      t.setAttribute("id", "evand-widget-event-registration");
-      const e = document.getElementsByTagName("script")[0];
-      e.parentNode.insertBefore(t, e);
-    }
+  head() {
+    return {
+      script: [
+        {
+          async: true,
+          id: "evand-widget-event-registration",
+          src: "https://widgets.evand.com/event-registration.js?load=1"
+        }
+      ]
+    };
   }
 };
 </script>
