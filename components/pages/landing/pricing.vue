8228c7b0956915603013f5f3dad420e817b1ef97
HamidNE <hamidne@mail.ru>
2019-08-25 00:00:36 +0430

    add link to ticket page from index page

--- components/pages/landing/pricing.vue
+++ components/pages/landing/pricing.vue
@@ -2,9 +2,7 @@
   <div id="pricing-section">
     <div class="container py-7">
       <div class="w-md-75 w-lg-50 text-center mx-md-auto mb-9">
-        <h2 class="text-primary mb-4">
-          خرید بلیت
-        </h2>
+        <h2 class="text-primary mb-4">خرید بلیت</h2>
         <p>یک روز با Vue.js و عاشقان کد‌نویسی</p>
       </div>
 
@@ -18,21 +16,25 @@
               <span class="display-4 font-weight-bold">رایگان</span>
             </span>
 
-            <hr class="opacity-md my-4">
+            <hr class="opacity-md my-4" />
 
             <div class="mb-5">
-              <p class="text-light">
-                دانش خود را با ما به اشتراک بگذارید و با علاقه‌مندان Vue.Js هم صحبت شوید.
-              </p>
+              <p
+                class="text-light"
+              >دانش خود را با ما به اشتراک بگذارید و با علاقه‌مندان Vue.Js هم صحبت شوید.</p>
             </div>
 
-            <a class="btn btn-white rounded-pill mb-2 mr-auto d-flex align-items-center" href="#">
+            <nuxt-link
+              :to="{name: 'ticket'}"
+              class="btn btn-white rounded-pill mb-2 mr-auto d-flex align-items-center"
+              href="#"
+            >
               خرید بلیت
               <span class="fas fa-arrow-left text-primary font-size-1 mr-2" />
-            </a>
+            </nuxt-link>
 
             <div class="position-absolute bottom-0 right-0 w-100 price-card__image">
-              <img class="img-fluid" src="~assets/img/vue-half.png" alt="Ticket Background">
+              <img class="img-fluid" src="~assets/img/vue-half.png" alt="Ticket Background" />
             </div>
           </div>
         </div>
@@ -42,42 +44,30 @@
             <div class="row">
               <div class="col-sm-6 mb-5">
                 <h4 class="h5">
-                  <div class="vuejs-icon d-inline-block" />
-                  شبکه‌سازی
+                  <div class="vuejs-icon d-inline-block" />شبکه‌سازی
                 </h4>
-                <p>
-                  با اعضای فعال و با‌تجربه جامعه Vue.js ایران آشنا شوید و روابط خود را با متخصصان این حوزه گسترش دهید.
-                </p>
+                <p>با اعضای فعال و با‌تجربه جامعه Vue.js ایران آشنا شوید و روابط خود را با متخصصان این حوزه گسترش دهید.</p>
               </div>
               <div class="col-sm-6 mb-5">
                 <h4 class="h5">
-                  <div class="vuejs-icon d-inline-block" />
-                  توسعه
+                  <div class="vuejs-icon d-inline-block" />توسعه
                 </h4>
-                <p>
-                  از دانش برنامه‌نویسان فعال و با‌تجربه این حوزه استفاده کنید و برای مسائل حل نشده خود، راه‌حل پیدا کنید.
-                </p>
+                <p>از دانش برنامه‌نویسان فعال و با‌تجربه این حوزه استفاده کنید و برای مسائل حل نشده خود، راه‌حل پیدا کنید.</p>
               </div>
             </div>
 
             <div class="row mb-9">
               <div class="col-sm-6">
                 <h4 class="h5">
-                  <div class="vuejs-icon d-inline-block" />
-                  اشتراک‌گذاری
+                  <div class="vuejs-icon d-inline-block" />اشتراک‌گذاری
                 </h4>
-                <p>
-                  از تجارب دیگر برنامه‌نویسان این حوزه استفاده کنید و با اشتراک‌‌گذاری تجارب شخصیتان، هم بیاموزید و هم آموزش دهید.
-                </p>
+                <p>از تجارب دیگر برنامه‌نویسان این حوزه استفاده کنید و با اشتراک‌‌گذاری تجارب شخصیتان، هم بیاموزید و هم آموزش دهید.</p>
               </div>
               <div class="col-sm-6 mb-5">
                 <h4 class="h5">
-                  <div class="vuejs-icon d-inline-block" />
-                  آینده‌نگری
+                  <div class="vuejs-icon d-inline-block" />آینده‌نگری
                 </h4>
-                <p>
-                  شانس شما برای یافتن یک فرصت شغلی ایده‌آل یا پروژه‌ای بزرگ به اندازه تعداد افرادی است که شما را می شناسند، پس آینده‌نگر باشید و شناخته شوید.
-                </p>
+                <p>شانس شما برای یافتن یک فرصت شغلی ایده‌آل یا پروژه‌ای بزرگ به اندازه تعداد افرادی است که شما را می شناسند، پس آینده‌نگر باشید و شناخته شوید.</p>
               </div>
             </div>
           </div>
@@ -88,20 +78,26 @@
 </template>
 
 <style scoped lang="scss">
-  $primaryColor: #40b984;
-  $secondaryColor: #30475e;
+$primaryColor: #40b984;
+$secondaryColor: #30475e;
 
-  #price-card {
-    background: $secondaryColor;
-  }
+#price-card {
+  background: $secondaryColor;
+}
 
-  .price-card__image {
-    max-width: 7rem;
-  }
+.price-card__image {
+  max-width: 7rem;
+}
 
-  .vuejs-icon {
-    width: 1rem;
-    height: 1rem;
-    background: repeating-linear-gradient(0deg, $primaryColor, $primaryColor .5rem, $secondaryColor .5rem, $secondaryColor 1rem);
-  }
+.vuejs-icon {
+  width: 1rem;
+  height: 1rem;
+  background: repeating-linear-gradient(
+    0deg,
+    $primaryColor,
+    $primaryColor 0.5rem,
+    $secondaryColor 0.5rem,
+    $secondaryColor 1rem
+  );
+}
 </style>
