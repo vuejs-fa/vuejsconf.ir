9d29d00fb801439bade5badbda51d50783eeb04f
HamidNE <hamidne@mail.ru>
2019-08-30 18:33:40 +0430

    add EvandWidget component to project

---
+++ components/pages/ticket/EvandWidget.vue
@@ -0,0 +1,31 @@
+<template>
+  <div
+    class="evand-widget evand-widget-event-registration my-7"
+    :data-event-slug="meetup.evandSlug"
+    data-setting
+  ></div>
+</template>
+
+<script>
+import meetup from "~/data/meetup";
+export default {
+  data() {
+    return {
+      meetup
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "~assets/scss/_variables";
+
+.evand-widget {
+  .powered-by-evand {
+    display: none;
+  }
+  .evand-button {
+    background: $secondary !important;
+  }
+}
+</style>
