5ecd19ca7ccf5bba8b5cf0e5f7b1b0b65bfa7632
HamidNE <hamidne@mail.ru>
2019-10-01 16:04:06 +0330

    replace host contents

--- components/pages/landing/host.vue
+++ components/pages/landing/host.vue
@@ -4,9 +4,9 @@
     class="parallax d-flex justify-content-center align-items-center"
   >
     <div class="content w-100 h-100 d-flex align-items-center justify-content-center flex-column text-white p-3">
-      <h2>میزبان، فضای کار اشتراکی زاویه</h2>
-      <p class="text-white">
-        زاویه یک فضای کاری اشتراکی برای کارآفرینان، فریلنسرها و افراد خلاق است.
+      <h2>میزبان، شرکت سفرهای علی‌بابا</h2>
+      <p class="text-white mt-3 font_12">
+        ساختمان روز اول علی‌بابا در کارخانه‌ی نوآوری آزادی، میزبان گردهمایی ماست
       </p>
     </div>
   </div>
@@ -15,7 +15,7 @@
 <style scoped lang="scss">
   .parallax {
     height: 500px;
-    background-image: url('~assets/img/zavie.jpg');
+    background-image: url('~assets/img/rooz-aval.jpg');
     background-attachment: fixed;
     background-position: center;
     background-repeat: no-repeat;
